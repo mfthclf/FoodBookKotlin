cd0904b71ec75ffd330ab6ff1f5815bfbb518d5a
Fatih Culfa <mfthclf@gmail.com>
2024-07-17 11:19:44 +0300

    User interfaces completed

---
+++ app/src/main/java/com/mfthc/foodbookkotlin/FoodDetailFragment.kt
@@ -0,0 +1,40 @@
+package com.mfthc.foodbookkotlin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mfthc.foodbookkotlin.databinding.FragmentFoodDetailBinding
+
+
+class FoodDetailFragment : Fragment() {
+
+    private var _binding: FragmentFoodDetailBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentFoodDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+}
