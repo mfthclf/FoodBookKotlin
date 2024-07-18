3e4f4a4b0835228ccfe9a0d4f854854415f15e18
Fatih Culfa <mfthclf@gmail.com>
2024-07-18 09:47:34 +0300

    Data Race problem solved

--- app/src/main/java/com/mfthc/foodbookkotlin/roomdb/FoodDB.kt
+++ app/src/main/java/com/mfthc/foodbookkotlin/roomdb/FoodDB.kt
@@ -1,10 +1,32 @@
 package com.mfthc.foodbookkotlin.roomdb
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.mfthc.foodbookkotlin.model.Food
 
 @Database(entities = [Food::class], version = 1)
 abstract class FoodDB : RoomDatabase() {
     abstract fun getDao(): FoodDAO
+
+    companion object {
+
+        @Volatile
+        private var instance: FoodDB? = null
+
+        private val lock = Any()
+
+        operator fun invoke(context: Context) = instance ?: synchronized(lock) {
+            instance ?: createDataBase(context).also {
+                instance = it
+            }
+        }
+
+        private fun createDataBase(context: Context) = Room.databaseBuilder(
+            context.applicationContext,
+            FoodDB::class.java,
+            "FoodDataBase"
+        ).build()
+    }
 }
