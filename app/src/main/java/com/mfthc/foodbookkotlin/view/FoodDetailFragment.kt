f2154c296e6f603c87a17ae5915e5f33ea410fd7
Fatih Culfa <mfthclf@gmail.com>
2024-07-19 15:13:45 +0300

    The display of the details of the foods has been completed

--- app/src/main/java/com/mfthc/foodbookkotlin/view/FoodDetailFragment.kt
+++ app/src/main/java/com/mfthc/foodbookkotlin/view/FoodDetailFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.mfthc.foodbookkotlin.databinding.FragmentFoodDetailBinding
+import com.mfthc.foodbookkotlin.util.downloadImage
+import com.mfthc.foodbookkotlin.util.makePlaceHolder
+import com.mfthc.foodbookkotlin.viewmodel.FoodDetailViewModel
 import kotlinx.coroutines.CoroutineScope
 
 
@@ -13,6 +17,8 @@ class FoodDetailFragment : Fragment() {
 
     private var _binding: FragmentFoodDetailBinding? = null
     private val binding get() = _binding!!
+    private lateinit var viewModel: FoodDetailViewModel
+    private var id = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,7 +36,28 @@ class FoodDetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this)[FoodDetailViewModel::class.java]
 
+        arguments?.let {
+            id = FoodDetailFragmentArgs.fromBundle(it).foodID
+        }
+        viewModel.getRoomData(id)
+
+        observeLiveData()
+
+    }
+
+    private fun observeLiveData() {
+        viewModel.foodLiveData.observe(viewLifecycleOwner) {
+            binding.foodImage.downloadImage(it.image, makePlaceHolder(requireContext()))
+            binding.foodName.text = it.name
+            binding.foodCalorie.text = it.calorie
+            binding.foodCarbohydrate.text = it.carbohydrate
+            binding.foodProtein.text = it.protein
+            binding.foodFat.text = it.fat
+
+
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/mfthc/foodbookkotlin/viewmodel/FoodDetailViewModel.kt
+++ app/src/main/java/com/mfthc/foodbookkotlin/viewmodel/FoodDetailViewModel.kt
@@ -1,6 +1,23 @@
 package com.mfthc.foodbookkotlin.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.mfthc.foodbookkotlin.model.Food
+import com.mfthc.foodbookkotlin.roomdb.FoodDB
+import kotlinx.coroutines.launch
 
-class FoodDetailViewModel {
 
+class FoodDetailViewModel(application: Application) : AndroidViewModel(application) {
+
+    val foodLiveData = MutableLiveData<Food>()
+
+    fun getRoomData(id:Int){
+        viewModelScope.launch {
+            val dao = FoodDB(getApplication()).getDao()
+            val food = dao.getFood(id)
+            foodLiveData.value=food
+        }
+    }
 }
